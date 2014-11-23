44aca93ab85916fa68b73aeb1c25c5c0856870cc
Mihai Rotaru <mihai.rotaru@gmx.com>
2014-11-23 11:03:10 +0000

    add debug and fix kill event

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var fs = require('fs');
 var program = require('commander');
 var path = require('path');
 var _ = require('lodash');
+var validator = require('validator');
+
+// debug and logging
+process.env.DEBUG='*';
+var debug = require('debug');
+var debugFiles = debug('files')
+var debugParser = debug('parser')
 
 program
   .version('0.0.1')
@@ -11,6 +18,7 @@ program
 var inputArg = program.args[0];
 var inputs = [];
 if(fs.lstatSync(inputArg).isFile()) {
+    debugFiles('loading: ' + inputArg);
     inputs = fs.readFileSync(inputArg).toString().split("\n");
 } else if(fs.lstatSync(inputArg).isDirectory()){
     // not supported yet
@@ -19,7 +27,7 @@ if(fs.lstatSync(inputArg).isFile()) {
 var events = [
     {
         type: "kill",
-        regex: "^((?:[\d]{2}:?){3})\ -\ ([^\s]+)?\s(?:<img=([\w]+)>)\s([^\s]+)",
+        regex: "^((?:[\\d]{2}:?){3})\\ -\\ ([^\\s]+)?\\s(?:<img=([\\w]+)>)\\s([^\\s]+)",
         properties: {
             "killer": "0",
             "icon"  : "1",
@@ -34,15 +42,18 @@ var events = [
     }
 ];
 
+var lineNumber = 0; // for debugging messages
+
 /**
  * Iterate over each line in the log file
  */
 _.each(inputs, function(line){
+    
+    line = validator.trim(line);
+    debugParser('processing: ' + line);
+    lineNumber++;
 
-    /**
-     * Set to 'true' when a match is found
-     */
-    var done = false;
+    var done = false; // set to 'true' when a match is found
 
     /**
      * See if the line matches any one of the known event types. When a match is
@@ -53,12 +64,14 @@ _.each(inputs, function(line){
         if(!done) {
             var re = new RegExp(eventType.regex);
             var matches = re.exec(line);
-            if(match){
+            if(matches){
+                debugParser('line: ' + lineNumber + ' matched "' + eventType.type + '" event.');
                 var event = {};
                 event.type = eventType.type;
                 var properties = _.mapValues(eventType.properties, function(index){
-                    return match[index];
+                    return matches[parseInt(index)+2];
                 });
+                console.log(properties);
                 done = true;
             }
         }
